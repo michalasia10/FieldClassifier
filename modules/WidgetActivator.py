345a0164c97079bf6ef51934b3bbf01eb7d391f9
michalasia10 <misiek1027@o2.pl>
2021-08-11 23:02:45 +0200

    * create class to change visibility of widgets

---
+++ modules/WidgetActivator.py
@@ -0,0 +1,19 @@
+class WidgetActivator:
+    def __init__(self, form, uniqueClasses):
+        self._form = form
+        self._uniqueClasses = uniqueClasses
+
+    def widget_activator(self, flag: bool = True):
+        form = self._form
+        widgetsForClass = {
+            1: form.firstClassW,
+            2: form.secondClassW,
+            3: form.thirdClassW,
+            4: form.fourthClassW,
+            5: form.fifthClassW,
+
+        }
+        form.statWidgets.setEnabled(flag)
+        form.graphWidget.setEnabled(flag)
+        for item in self._uniqueClasses:
+            widgetsForClass[item].setEnabled(flag)
