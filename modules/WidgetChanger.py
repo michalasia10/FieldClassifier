54b0956773117e0206629bab6d8877e57431d3ec
michalasia10 <misiek1027@o2.pl>
2021-08-11 23:29:40 +0200

    * change methods to be static

--- modules/WidgetChanger.py
+++ modules/WidgetChanger.py
@@ -5,6 +5,14 @@ class WidgetChanger:
         self._form = form
         self._scene = self._form.graphScene
 
+    @staticmethod
+    def list_drainer(dirtyLists):
+        return [[] for _ in dirtyLists]
+
+    @staticmethod
+    def deafult_float_values(variables: list):
+        return tuple([0.0 for _ in variables])
+
     def clean_scene(self):
         if self._scene.scene() is not None:
             self._scene.scene().clear()
@@ -16,14 +24,10 @@ class WidgetChanger:
     def reset_widget(self,widget,value):
         widget.setText(value)
 
-    def deafult_float_values(self,variables:list):
-        return tuple([0.0 for _ in variables])
 
     def set_value_in_widgets(self,widgets:list,value):
         for widget in widgets:
             widget.setText(value)
 
-    def list_drainer(self,dirtyLists):
-        return [[] for _ in dirtyLists]
 
 
